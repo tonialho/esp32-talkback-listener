71f85d61107350ea417b0300fee98ea480096025
Toni Alho <toni.alho24@gmail.com>
2022-01-13 18:47:49 +0200

    Add config.h template

---
+++ TEMPLATE_config.h
@@ -0,0 +1,3 @@
+#define WIFI_SSID "mynameisnetwork"
+#define WIFI_PASSWORD "password"
+#define TALKBACK_URL "https://api.thingspeak.com/talkbacks/<TALKBACK ID>/commands/<COMMAND ID>.json?api_key=<APIKEY>"
